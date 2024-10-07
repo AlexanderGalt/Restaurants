8b527aca40fe675d15674fd115b983eea5dc6918
AlexanderGalt <alexander.galt@mail.ru>
2024-10-07 07:52:15 +0200

    fix: ProgressBar formula

--- src/widgets/ProgressBar.jsx
+++ src/widgets/ProgressBar.jsx
@@ -1,47 +1,35 @@
 import { useEffect } from "react"
 import { useState } from "react"
 
-function calcPercentage() {
-	return (
-		((document.documentElement.clientHeight + window.scrollY) / document.documentElement.scrollHeight) * 100
-	).toFixed(0)
-}
-
 export function ProgressBar() {
-	const [progressScrollBarValue, setProgressScrollBarValue] = useState(); // если в "useState" указать в параметр "initialValue" вызов функции "calcPercentage()" и не вызывать отдельно "handleScroll()" в "useEffect()", то будет баг:
-	// при открытии страницы значение прогресс-бара будет некорректным, т.к. возьмётся из параметра "initialValue", т.е. будет "calcPercentage()", но при открытии страницы ещё не все элементы ДОМа отрисовались, реакт не успевает их зарендерить. В следствии чего, на моммент подсчёта формулой "calcPercentage()", высота нашей страницы меньше, чем должна быть на самом деле (т.к. не всё ещё успело отрисоваться). Поэтому при открытии стрницы мы полчим не 70% прогресс бара (как должно быть), а 95% (т.к. страница ещё не успела вся зарендерится и занять долнжную высоту). Т.е. нам нужно считать значерние прогресбара только ПОСЛЕ того как все компаненты замаунтились (смантировались, зарендерились в ДОМ). Для этого необходимо использовать "useEffect()".
+	const [progressScrollBar, setProgressScrollBar] = useState(0); // 0 - значение прогрес-скролбара при открытии страницы.
 
 	useEffect(() => {
-		const handleScroll = () => setProgressScrollBarValue(calcPercentage());
-		handleScroll(); // если этого вызова не будет, то при открытии страницы, значение прогресс-бара будет некорректным, т.к. коректное значение посчитается только при скроле. Т.е. без этой строчки нам будет необходимо проскролить страницу чтобы увидитеть корректное значение прогресс-бара.
-		document.addEventListener('scroll', handleScroll);
+		const handleScroll = () => {
+			const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+			const documentHeight = scrollHeight - clientHeight; // вся высота документа, которую нам можно проскролить.
+			setProgressScrollBar((scrollTop / documentHeight) * 100);
+		};
+		window.addEventListener('scroll', handleScroll);
 
-		return () => {
-			document.removeEventListener("scroll", handleScroll);
-		}
+		return () => window.removeEventListener('scroll', handleScroll); // очистка события при размонтировании компонента
 	}, []);
 
 	return (
 		<div
 			style={{
-				position: "fixed",
+				position: 'fixed',
 				top: 0,
 				left: 0,
+				height: '16px',
 				width: "100%",
-				height: "20px",
-				background: "#000",
-			}}
-			className="progress-scroll-bar"
-		>
-			<div
-				style={{
-					width: progressScrollBarValue + "%",
-					height: "100%",
-					background: "red",
-				}}
-				className="progress-scroll-bar__value">
-
-			</div>
+			}}>
+			<div style={{
+				width: `${progressScrollBar}%`,
+				background: "red",
+				transition: '0.3s ease-out',
+				height: "100%"
+			}}></div>
 		</div>
-	)
-}
+	);
+};
